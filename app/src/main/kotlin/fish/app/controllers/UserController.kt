25cf0147b724b929ef391173911878e4c8547716
adrodin <adrian.dochnal@student.pk.edu.pl>
2022-01-07 18:47:38 +0100

    add hibernate dependency

--- app/src/main/kotlin/fish/app/controllers/UserController.kt
+++ app/src/main/kotlin/fish/app/controllers/UserController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("user")
 class UserController {
